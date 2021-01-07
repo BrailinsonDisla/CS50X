fbfdc5cba8df184e8a92198fa969e5840732ab06
Brailinson Disla <Brailinson.Disla@gmail.com>
2021-01-07 14:38:27 -0500

    restructure

---
+++ Projects/2. Hello World/hello.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *  Name: Brailinson Disla
+ *
+ *  Compilation:  make hello
+ *  Execution:    ./hello
+ *
+ *
+ *  % ./hello
+ ******************************************************************************/
+#include <stdio.h>
+
+// max number of characters constant
+const int MAX_CHARS = 20;
+
+int main(void) {
+    // asks the user for name
+    printf("What's your name? ");
+
+    // scans and stores the name as a char[]
+    char name[MAX_CHARS];
+    scanf("%[^\n]%*c", name);
+
+
+    // prints 'hello, *name*'
+    printf("hello, %s\n", name);
+
+    // exit
+    return 0;
+}
+
+/******************************************************************************
+ *  Notes:
+ *    - main() or main(int argc, char** argv) is the entry point
+ *      + the return statemnt indicates the sucessfulness of the program
+ *          * by convention
+ *            - return value of 0 indicates successfulness
+ *            - return value of any non-zero number indicates failure
+ *          * return can be omitted for main
+ *    - in scanf(...), %[^\n] defines a new line as delimeter
+ ******************************************************************************/
