bf8499f8e8ff43e0cb5e0baaa8d9a86d2acd8780
Brailinson Disla <Brailinson.Disla@gmail.com>
2021-02-10 01:01:03 -0500

    completed

--- Projects/6. Caesar/caesar.c
+++ Projects/6. Caesar/caesar.c
@@ -10,90 +10,71 @@
  ******************************************************************************/
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // prototypes for user-defined functions
 int usage();
+void encrypt(char*, char*, int);
+
+// global variables
+const int MAX_BUFFER = 4096;
 
 int main(int argc, char **argv) {
-    // max text character length
-    const int MAX_CHARS = 5000;
-
-    // constants for en- and de-cryption
-    const int a = (int) 'a', A = (int) 'A',
-                z = (int) 'z', Z = (int) 'Z';
-
-    // constant for alphabetic range
-    const int range = z - a + 1;
-
-    // stores the key
-    int k = -1;
-
-    // checks for correct usage format
+    // ensure a key was passed
     if (argc != 2) {
-        // prints usage information
+        // remind user of usage
         return usage();
-    } else {
-        // sets the key to given key
-        k = atoi(argv[1]);
-//-------------------------------------
-        // make sure it is not a string
-        if (k <= 0) {
-            // prints usage information
-            return usage();
-        }
     }
 
-    // calculate offset
-    int offset = k % range;
+    // integer for key
+    int key = atoi(argv[1]);
 
-    // prompts the user for plaintext
+    // ensure the key is valid
+    if (key <= 0) {
+        // remind user of usage
+        return usage();
+    }
+
+    // string for message
+    char message[MAX_BUFFER];
+
+    // ask for plaintext
     printf("plaintext: ");
 
-    // scans and stores the plaintext as a char[]
-    char plaintext[MAX_CHARS];
-    scanf("%[^\n]%*c", plaintext);
+    // scan message input
+    scanf("%[^\n]%*c", message);
 
-    // calculates the length of the text
-    int length = strlen(plaintext);
+    // string for ciphertext
+    char ciphertext[MAX_BUFFER];
 
-    // allocates space for the encrypted text
-    char ciphertext[length];
+    // encrypt message
+    encrypt(message, ciphertext, key);
 
-    // encrypts the plaintext
-    for (int i = 0; i < length; i++) {
-        // stores the current character in iteration
-        char c = plaintext[i];
-
-        // case one: for uppercase characters
-        if (c >= A && c <= Z) {
-            // encrypt the character
-            ciphertext[i] = (char) (c + (c + offset) % Z);
-        }
-
-        // case two: for lowercase characters
-        else if (c >= a && c <= a) {
-            // encrypt the character
-            ciphertext[i] = (char) (c + (c + (c + offset) % z));
-        }
-
-        // case three: non-alpha character
-        else {
-            // leave the character
-            ciphertext[i] = c;
-        }
-    }
-
-    // print ciphertext
-    printf("%s\n", ciphertext);
-
-    // helper code
-    printf("key %i\n", k);
-    printf("a: %i\nA: %i\n", 'a', 'A');
-    printf("z: %i\nZ: %i\n", 'z', 'Z');
+    // print encrypted text
+    printf("ciphertext: %s\n", ciphertext);
 }
 
+// encrypts message
+void encrypt(char* message, char* ciphertext, int key) {
+    // calculate string length
+    int length = strlen(message);
 
-// prints ysage information
+    // iterate through message
+    for (int i = 0; i <= length; i++) {
+        if (message[i] >= 'A' && message[i] <= 'Z') {
+            // encrypt uppercase alpha
+            ciphertext[i] = 'A' + (message[i] - 'A' + key) % 26;
+        } else if (message[i] >= 'a' && message[i] <= 'z') {
+            // encrypt lowercase alpha
+            ciphertext[i] = 'a' + (message[i] - 'a' + key) % 26;
+        } else {
+            // do not encrypt non-alpha
+            ciphertext[i] = message[i];
+        }
+    }
+}
+
+// prints usage information
 int usage() {
     // informs the user of appropriate usage format
     printf("Usage: ./caesar key\n");
